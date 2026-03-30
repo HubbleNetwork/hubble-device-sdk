a2ca68fede56521a67ad0173df293ca1c301266d
Flavio Ceolin <flavio@hubble.com>
2026-03-30 12:49:15 -0700

    samples: freertos/beacon: Sample uses its own configuration
    The test has its own configuration file.
    
    Signed-off-by: Flavio Ceolin <flavio@hubble.com>

---
+++ samples/freertos/ti/ble-beacon/src/hubble_config.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2026 Hubble Network, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SRC_HUBBLE_CONFIG_H
+#define SRC_HUBBLE_CONFIG_H
+
+#define CONFIG_HUBBLE_BLE_NETWORK             1
+#define CONFIG_HUBBLE_SAT_NETWORK             0
+
+#define CONFIG_HUBBLE_KEY_SIZE                16
+
+#define CONFIG_HUBBLE_EID_TIME_BASED          1
+#define CONFIG_HUBBLE_EID_ROTATION_PERIOD_SEC 86400
+
+#endif /* SRC_HUBBLE_CONFIG_H */
