a20ee25837fb50b659515a22bf91aaae517c6400
Flavio Ceolin <flavio@hubble.com>
2026-09-24 00:18:46 -0700

    port: nordic: propagate packet send error in board wrappers
    The thingy53 and nrf21540dk board wrappers discarded the return of
    hubble_sat_soc_packet_send() and always returned 0, hiding errors
    (-EIO/-EAGAIN) from the caller.
    
    Signed-off-by: Flavio Ceolin <flavio@hubble.com>

--- port/zephyr/boards/nordic/nrf52/nrf21540dk.c
+++ port/zephyr/boards/nordic/nrf52/nrf21540dk.c
@@ -34,11 +34,13 @@ int hubble_sat_board_disable(void)
 
 int hubble_sat_board_packet_send(const struct hubble_sat_packet_frames *packet)
 {
+	int ret;
+
 	hubble_board_fem_enable();
-	hubble_sat_soc_packet_send(packet);
+	ret = hubble_sat_soc_packet_send(packet);
 	hubble_board_fem_sleep();
 
-	return 0;
+	return ret;
 }
 
 #ifdef CONFIG_HUBBLE_SAT_NETWORK_DTM_MODE
